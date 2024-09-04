41982a4f9589f01577ae1a47b1dfddd68a76f220
kuvala-12 <kuvala.s2021it@sece.ac.in>
2024-09-04 10:08:00 +0530

    first commit

---
+++ user.js
@@ -0,0 +1,60 @@
+
+module.exports=
+[{id:1,flightname:"Air Asia India",from:"New Delhi",to:"Mumbai",price:"4000/-",Date:"21/07/2023",image:"image.jpg"},
+{id:2,flightname:"Air Asia India ",from:"New Delhi",to:"Mumbai",price:"3000/-",Date:"21/07/2023",image:"image.jpg"},
+{id:3,flightname:"Air Asia India",from:"New Delhi",to:"Mumbai",price:"2500/-",Date:"21/07/2023",image:"image.jpg"},
+{id:4,flightname:"Air Asia India",from:"New Delhi",to:"Mumbai",price:"5000/-",Date:"21/07/2023",image:"image.jpg"},
+{id:5,flightname:"Air Asia India",from:"New Delhi",to:"Mumbai",price:"5000/-",Date:"21/07/2023",image:"image.jpg"},
+
+// {id:6,flightname:"Indigo",from:"New Delhi",to:"Mumbai",price:"4000/-",hours:"02 h 25 m",image:"image.jpg"},
+// {id:7,flightname:"Indigo",from:"Pune",to:"Bangalore",price:"3000/-",hours:"01 h 50 m",image:"image.jpg"},
+// {id:8,flightname:"Indigo",from:"New Delhi",to:"Mumbai",price:"2500/-",hours:"03 h 45 m",image:"image.jpg"},
+// {id:9,flightname:"Indigo",from:"Mumbai",to:"Coimbatore",price:"5000/-",hours:"05 h 15 m",image:"image.jpg"},
+// {id:10,flightname:"Indigo",from:"Mumbai",to:"Coimbatore",price:"5000/-",hours:"05 h 15 m",image:"image.jpg"},
+
+
+// {id:11,flightname:"Akasa Air",from:"New Delhi",to:"Mumbai",price:"4000/-",hours:"02 h 25 m",image:"image.jpg"},
+// {id:12,flightname:"Akasa Air",from:"Pune",to:"Bangalore",price:"3000/-",hours:"01 h 50 m",image:"image.jpg"},
+// {id:13,flightname:"Akasa Air",from:"New Delhi",to:"Mumbai",price:"2500/-",hours:"03 h 45 m",image:"image.jpg"},
+// {id:14,flightname:"Akasa Air",from:"Mumbai",to:"Coimbatore",price:"5000/-",hours:"05 h 15 m",image:"image.jpg"},
+// {id:15,flightname:"Akasa Air",from:"Mumbai",to:"Coimbatore",price:"5000/-",hours:"05 h 15 m",image:"image.jpg"},
+
+
+
+// {id:16,flightname:"Vistara Airline",from:"New Delhi",to:"Mumbai",price:"4000/-",hours:"02 h 25 m",image:"image.jpg"},
+// {id:17,flightname:"Vistara Airline",from:"Pune",to:"Bangalore",price:"3000/-",hours:"01 h 50 m",image:"image.jpg"},
+// {id:18,flightname:"Vistara Airline",from:"New Delhi",to:"Mumbai",price:"2500/-",hours:"03 h 45 m",image:"image.jpg"},
+// {id:19,flightname:"Vistara Airline",from:"Mumbai",to:"Coimbatore",price:"5000/-",hours:"05 h 15 m",image:"image.jpg"},
+// {id:20,flightname:"Vistara Airline",from:"Mumbai",to:"Coimbatore",price:"5000/-",hours:"05 h 15 m",image:"image.jpg"},
+
+
+
+
+// {id:21,flightname:"Air India",from:"New Delhi",to:"Mumbai",price:"4000/-",hours:"02 h 25 m",image:"image.jpg"},
+// {id:22,flightname:"Air India",from:"Pune",to:"Bangalore",price:"3000/-",hours:"01 h 50 m",image:"image.jpg"},
+// {id:23,flightname:"Air India",from:"New Delhi",to:"Mumbai",price:"2500/-",hours:"03 h 45 m",image:"image.jpg"},
+// {id:24,flightname:"Air India",from:"Mumbai",to:"Coimbatore",price:"5000/-",hours:"05 h 15 m",image:"image.jpg"},
+// {id:25,flightname:"Air India",from:"Mumbai",to:"Coimbatore",price:"5000/-",hours:"05 h 15 m",image:"image.jpg"},
+
+
+
+
+// {id:26,flightname:"SpiceJet",from:"New Delhi",to:"Mumbai",price:"4000/-",hours:"02 h 25 m",image:"image.jpg"},
+// {id:27,flightname:"SpiceJet",from:"Pune",to:"Bangalore",price:"3000/-",hours:"01 h 50 m",image:"image.jpg"},
+// {id:28,flightname:"SpiceJet",from:"New Delhi",to:"Mumbai",price:"2500/-",hours:"03 h 45 m",image:"image.jpg"},
+// {id:29,flightname:"SpiceJet",from:"Mumbai",to:"Coimbatore",price:"5000/-",hours:"05 h 15 m",image:"image.jpg"},
+// {id:30,flightname:"SpiceJet",from:"Mumbai",to:"Coimbatore",price:"5000/-",hours:"05 h 15 m",image:"image.jpg"},
+
+
+
+
+
+// {id:31,flightname:"Air Asia India",from:"New Delhi",to:"Mumbai",price:"4000/-",hours:"02 h 25 m",image:"image.jpg"},
+// {id:32,flightname:"Indigo",from:"Pune",to:"Bangalore",price:"3000/-",hours:"01 h 50 m",image:"image.jpg"},
+// {id:33,flightname:"Akasa AIr",from:"New Delhi",to:"Mumbai",price:"2500/-",hours:"03 h 45 m",image:"image.jpg"},
+// {id:34,flightname:"Vistara Airline",from:"Mumbai",to:"Coimbatore",price:"5000/-",hours:"05 h 15 m",image:"image.jpg"},
+// {id:35,flightname:"Air India",from:"Mumbai",to:"Coimbatore",price:"5000/-",hours:"05 h 15 m",image:"image.jpg"},
+
+
+
+]
